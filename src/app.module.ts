8fba47b9d7a2e8135b0d3c54bf0021256083c6e8
Vadym Zaiats <vadyk.za@gmail.com>
2024-04-17 13:50:54 +0300

    light ref

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
-import { PostsModule } from './posts/posts.module';
-import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { Post } from './posts/entities/post.entity';
+import { UsersController } from './users/users.controller';
+import { PostsController } from './posts/posts.controller';
+import { PostsService } from './posts/posts.service';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DbModule,
-    PostsModule,
-    UsersModule,
+    TypeOrmModule.forFeature([User, Post]),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController, PostsController],
+  providers: [AppService, UsersService, PostsService],
 })
 export class AppModule {}
